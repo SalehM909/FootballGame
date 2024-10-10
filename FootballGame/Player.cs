06eb15de22029a2aee1b892279e839ba9a4f1091
SalehM909 <saleh.m.alshabibi@gmail.com>
2024-10-10 09:14:15 +0400

    Add project files.

---
+++ FootballGame/Player.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballGame
+{
+    public enum Position // using this options only
+    {
+        Forward,
+        Midfielder,
+        Defender,
+        Goalkeeper
+    }
+
+    // Player class
+    public class Player
+    {
+        public string Name { get; set; }
+        public Position Position { get; set; } // using what I write in position
+        public int SkillsLevel { get; set; }
+
+        public Player(string name, Position position, int skillsLevel)// constructor method that takes three parameters
+        {
+            Name = name;
+            Position = position;
+            SkillsLevel = skillsLevel;
+        }
+    }
+}
