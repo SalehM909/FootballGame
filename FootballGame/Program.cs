5f08086d7088fc3e4e3488cea24d0b8f471bcadf
SalehM909 <saleh.m.alshabibi@gmail.com>
2024-10-10 12:02:13 +0400

    Generate players, Display teams and Start match

--- FootballGame/Program.cs
+++ FootballGame/Program.cs
@@ -2,17 +2,42 @@
 {
     public class Program
     {
-
-
-        static void Main(string[] args)
+        public class GameSimulator
         {
-            Console.WriteLine("Welcome to the Soccer Game Simulator!");
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Welcome to the Soccer Game Simulator!");
 
-            // Input team names
-            Console.Write("Enter Name For First Team : ");
-            string team1Name = Console.ReadLine();
-            Console.Write("Enter Name For Second Team : ");
-            string team2Name = Console.ReadLine();
+                // Input team names
+                Console.Write("Enter Name For First Team : ");
+                string team1Name = Console.ReadLine();
+                Console.Write("Enter Name For Second Team : ");
+                string team2Name = Console.ReadLine();
+
+                // Create teams
+                Team team1 = new Team(team1Name);
+                Team team2 = new Team(team2Name);
+
+                // Generate players
+                Console.WriteLine("Generating Players For Both Teams...");
+                GeneratePlayers(team1, team1Name);
+                GeneratePlayers(team2, team2Name);
+
+                // Display teams
+                DisplayTeam(team1);
+                DisplayTeam(team2);
+
+                // Start match
+                Match match = new Match(team1, team2);
+                match.PlayMatch();
+            }
+
         }
+
+
+
+
+
     }
+    
 }
