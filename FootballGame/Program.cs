5888a966a563fc5908ed560f52eab7b8173ec04f
SalehM909 <saleh.m.alshabibi@gmail.com>
2024-10-10 11:57:43 +0400

    Input team names

--- FootballGame/Program.cs
+++ FootballGame/Program.cs
@@ -2,9 +2,17 @@
 {
     public class Program
     {
+
+
         static void Main(string[] args)
         {
-           
+            Console.WriteLine("Welcome to the Soccer Game Simulator!");
+
+            // Input team names
+            Console.Write("Enter Name For First Team : ");
+            string team1Name = Console.ReadLine();
+            Console.Write("Enter Name For Second Team : ");
+            string team2Name = Console.ReadLine();
         }
     }
 }
