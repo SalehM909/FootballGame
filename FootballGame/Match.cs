2baf64f6c84f9a2ea5be0138d9f072747fede9b3
SalehM909 <saleh.m.alshabibi@gmail.com>
2024-10-10 11:09:43 +0400

    Add Match class withe constructors and simulate coin toss

--- FootballGame/Match.cs
+++ FootballGame/Match.cs
@@ -30,27 +30,5 @@ namespace FootballGame
             return random.Next(0, 2) == 0 ? Team1 : Team2;//( between 0 and 2)it can return either 0 or 1 //  If its 0, it returns Team1; otherwise, it returns Team2
         }
 
-        // Play a turn
-        private void PlayTurn(Team attackingTeam, Team defendingTeam)
-        {
-            int attackSkill = attackingTeam.CalculateAttackSkill();
-            int defenseSkill = defendingTeam.CalculateDefenseSkill();
-
-            if (attackSkill > defenseSkill)
-            {
-                Console.WriteLine($"{attackingTeam.Name} are attacking now......>>>>>> Goal!\n");
-                if (attackingTeam == Team1) Score1++;
-                else Score2++;
-            }
-            else
-            {
-                Console.WriteLine($"{attackingTeam.Name} are attacking now......>>>>>> Defended successfully!\n");
-            }
-
-            
-            Console.WriteLine($"Current Score: {Team1.Name}: {Score1} | {Team2.Name}: {Score2}");// Display current score after each turn
-        }
-
-
     }
 }
