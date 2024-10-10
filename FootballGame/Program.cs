fe323c9debb26547350f0a8c92b4a10015d0a070
SalehM909 <saleh.m.alshabibi@gmail.com>
2024-10-10 12:28:41 +0400

    use switch for player names, posistion and skills level

--- FootballGame/Program.cs
+++ FootballGame/Program.cs
@@ -6,7 +6,7 @@
         {
             static void Main(string[] args)
             {
-                Console.WriteLine("Welcome to the Soccer Game Simulator!\n");
+                Console.WriteLine("Welcome to the Soccer Game Simulator!");
 
                 // Input team names
                 Console.Write("Enter Name For First Team : ");
@@ -32,24 +32,45 @@
                 match.PlayMatch();
             }
 
-            
+
             private static void GeneratePlayers(Team team, string teamName)// generate 11 players for a team
             {
                 Random random = new Random();
 
-                
+
                 string[] RealMadridTeam = { "Thibaut Courtois", "Dani Carvajal", "Eder Militão", "David Alaba", "Ferland Mendy",
                                       "Casemiro", "Luka Modrić", "Toni Kroos", "Vinícius Júnior", "Karim Benzema",// Player names for Real Madrid
                                       "Rodrygo" };
 
-                
-               
+
+                string[] BarcelonaTeam = { "Marc-André ter Stegen", "Sergi Roberto", "Gerard Piqué", "Jordi Alba",
+                                     "Eric García", "Sergio Busquets", "Frenkie de Jong", "Gavi",
+                                     "Ansu Fati", "Robert Lewandowski", "Ousmane Dembélé" };// Player names for Barcelona
+
+                string[] names = teamName switch
+                {
+                    "Real Madrid" => RealMadridTeam,
+                    "Barcelona" => BarcelonaTeam,
+                    _ => RealMadridTeam // Default to Real Madrid names for any other team
+                };
+
+                Position[] positions = { Position.Forward, Position.Midfielder, Position.Defender, Position.Goalkeeper };
+                for (int i = 0; i < 11; i++)
+                {
+                    string name = names[random.Next(names.Length)];
+                    Position position = positions[i % positions.Length]; // Distribute positions evenly
+                    int skill = random.Next(80, 101); // Skill level between 80 and 100
+                    team.Players.Add(new Player(name, position, skill));
+                }
+            }
+
+
+
+
+
+
+
         }
-
-
-
-
-
     }
     
 }
